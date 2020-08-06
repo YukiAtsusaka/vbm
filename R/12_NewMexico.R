1f5f885ad2e38686d4a8db5f524b6913b3f75145
Mona Lisa <yukia887@gmail.com>
2020-08-06 09:34:03 +0900

    Construct 12-16 data

--- R/12_NewMexico.R
+++ R/12_NewMexico.R
@@ -7,6 +7,8 @@
 ################################################################################################
 
 ################################################################################################
+# (1) RACE CODING FOR NEW MEXICO DATA
+################################################################################################
 rm(list=ls())
 library(tidyverse)
 library(haven)
@@ -48,13 +50,56 @@ out <- predict_race(df, surname.year=2010, surname.only=T)
 
  df <- df %>% dplyr::select(voterID, est.race)
  
-write_csv(df, "NewMexico_Race.csv") 
+write_csv(df, "NewMexico_Race.csv")
+
+################################################################################################
+# (2) DATA BUILDING FOR NEW MEXICO
+################################################################################################
+rm(list=ls())
+library(tidyverse)
+library(haven)
+library(lubridate)
+
+estrace <- read_csv("NewMexico_Race.csv")
+dat <- read_dta("new mexico voter file 2017.dta")
+
+# ONLY KEEP THOSE WHO REGISTERED BTW 2012-2016
+dat2 <- dat %>% mutate(voterID = text_registrant_id,
+                       female =  ifelse(cde_gender=="F",1,0),
+                       democrat = ifelse(desc_party=="DEMOCRAT",1,0),
+                       birthyear = date_of_birth,
+                       R_date = mdy(date_of_registration),
+                       Reg_bf16 = ifelse(R_date <= mdy(11082016),1,0),
+                       Reg_in12 = ifelse(voting_method_pr2012!="",1,0),
+                       population = ifelse(Reg_bf16==1 & Reg_in12==1,1,0),
+                       voted2016 = ifelse(voting_method_pr2016!="" &      # DOUBLE CHECK THIS 8/6/20
+                                          voting_method_pr2016!="N",1,0),
+                       voted2012 = ifelse(voting_method_pr2012!="" &      # DOUBLE CHECK THIS 8/6/20
+                                          voting_method_pr2012!="N",1,0), 
+                       State = "New Mexico") %>% 
+       left_join(estrace, by="voterID") %>% 
+       mutate(estrace = est.race) %>%
+       dplyr::select(voterID, voted2012, voted2016, female, democrat, estrace, State, birthyear)
+
+
+nm12 <- dat2 %>% mutate(Vote = voted2012, age = 2012 - birthyear, Year=2012) %>%
+               dplyr:: select(voterID, female, democrat, age, estrace, State, Vote)
+  
+nm16 <- dat2 %>% mutate(Vote = voted2016, age = 2016 - birthyear, Year=16) %>%
+               dplyr:: select(voterID, female, democrat, age, estrace, State, Vote)
+  
+nm12_16 <- rbind(nm12,nm16)
+write_csv(nm12_16, "Stack_NM_2012_2016.csv")
 
 
 
-
-
-
+# CHECK
+#> table(dat$voting_method_pr2016)
+#
+#            A      E      N      P      Y 
+#342355  20802 117969 627512 187528      6 
+#--> ""=No Data, A=Absentee Voting, E=Early Voting, N=Not Turnout?
+#    P=Polling Place, Y=?
 
 
 ################################################################################################
