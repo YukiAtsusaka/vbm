5504ee4bc5c275a6290c222bb48bfd0202bf76ee
Mona Lisa <yukia887@gmail.com>
2020-10-03 13:14:04 +0900

    Add New Mexico

--- R/04_ParallelTrends.R
+++ R/04_ParallelTrends.R
@@ -38,27 +38,52 @@ dev.off()
 # VEP Highest Office Turnout is obtained from (http://www.electproject.org/home/voter-turnout/voter-turnout-data)
 
 year <- rep(seq(from=2000, to=2018, by=2), 2)
-state <- c(rep("Colorado", 10), rep("North Carolina",10))
 vep.colo <- c(57.5, 46.3, 66.7, 47.3, 71.0, 50.6, 69.9, 53.7, 70.0, 61.5)*0.01
 vep.nc   <- c(50.7, 39.7, 57.8, 31.0, 65.5, 39.2, 64.8, 40.8, 64.5, 48.7)*0.01
-turnout <- c(vep.colo, vep.nc)
-
-usep <- tibble(year, state, turnout)
+vep.nm   <- c(48.5, 38.6, 59.0, 42.5, 60.9, 42.8, 54.6, 35.4, 54.5, 46.5)*0.01
 xtick <- seq(from=2000, to=2018, by=2)
 
-pdf("Paralell.pdf", width=8, height=7)
+
+{
+pdf("Paralell.pdf", width=12, height=5.5)
+par(mar=c(2,2,1,0.5), oma = c(0,3,2,0) + 0.1, mfrow=c(1,2))
+
+# PLOT FOR NORTH CAROLINA
+state <- c(rep("Colorado", 10), rep("North Carolina",10))
+turnout <- c(vep.colo, vep.nc)
+usep <- tibble(year, state, turnout)
+
 plot(usep$turnout ~ usep$year, type="n", xaxt="n", xlab="Year", ylab="Voter turnout", ylim=c(0.3, 1), xlim=c(2000, 2018))
-axis(side=1, at=xtick, labels = FALSE)
-text(x=xtick,  par("usr")[3], labels = xtick, srt = 0, pos = 1, xpd = TRUE)
+axis(1, at=xtick, las=1, labels=xtick,cex.axis=1, mgp=c(0,0.8,0))
 lines(usep$turnout[usep$state=="Colorado"] ~ usep$year[usep$state=="Colorado"], col="gray", lwd=2)
-lines(usep$turnout[usep$state!="Colorado"] ~ usep$year[usep$state!="Colorado"], col="gray", lwd=2, lty=4)
+lines(usep$turnout[usep$state!="Colorado"] ~ usep$year[usep$state!="Colorado"], col="gray", lwd=2, lty=2)
 lines(usep$turnout[usep$state=="Colorado"&usep$year < 2013] ~ usep$year[usep$state=="Colorado"&usep$year < 2013], lwd=2)
-lines(usep$turnout[usep$state!="Colorado"&usep$year < 2013] ~ usep$year[usep$state!="Colorado"&usep$year < 2013], lwd=2, lty=4)
-abline(v=2013, lty=3)
-arrows(x0=2012, y0=0.85, x1=2012.9, y1=0.85, length=0.08) 
-text(x=2009.5, y=0.85, labels="VBM Adoption 03'")   
-legend("topright", legend=c("Colorado", "North Carolina"), lty=c(1, 4))
+lines(usep$turnout[usep$state!="Colorado"&usep$year < 2013] ~ usep$year[usep$state!="Colorado"&usep$year < 2013], lwd=2, lty=2)
+abline(v=2013, lty=1.2, lwd=1.5, col="firebrick4")
+arrows(x0=2011.5, y0=0.85, x1=2012.4, y1=0.85, length=0.08) 
+text(x=2009, y=0.85, labels="VBM Adoption")   
+legend("topleft", legend=c("Colorado", "North Carolina"), cex=1.2, lty=c(1,2), lwd=2, box.lty=0)
+
+
+# PLOT FOR NEW MEXICO
+state <- c(rep("Colorado", 10), rep("New Mexico",10))
+turnout <- c(vep.colo, vep.nm)
+usep <- tibble(year, state, turnout)
+
+plot(usep$turnout ~ usep$year, type="n", xaxt="n", xlab="Year", ylab="Voter turnout", ylim=c(0.3, 1), xlim=c(2000, 2018))
+axis(1, at=xtick, las=1, labels=xtick,cex.axis=1, mgp=c(0,0.8,0))
+
+lines(usep$turnout[usep$state=="Colorado"] ~ usep$year[usep$state=="Colorado"], col="gray", lwd=2)
+lines(usep$turnout[usep$state!="Colorado"] ~ usep$year[usep$state!="Colorado"], col="gray", lwd=2, lty=2)
+lines(usep$turnout[usep$state=="Colorado"&usep$year < 2013] ~ usep$year[usep$state=="Colorado"&usep$year < 2013], lwd=2)
+lines(usep$turnout[usep$state!="Colorado"&usep$year < 2013] ~ usep$year[usep$state!="Colorado"&usep$year < 2013], lwd=2, lty=2)
+abline(v=2013, lty=1.2, lwd=1.5, col="firebrick4")
+arrows(x0=2011.5, y0=0.85, x1=2012.4, y1=0.85, length=0.08) 
+text(x=2009, y=0.85, labels="VBM Adoption")   
+legend("topleft", legend=c("Colorado", "New Mexico"), cex=1.2, lty=c(1,2), lwd=2, box.lty=0)
 dev.off()
+}
+
 
 
 
