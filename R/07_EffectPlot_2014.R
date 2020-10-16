0fc217340b9030c166d8996d35294a81e340d467
Mona Lisa <yukia887@gmail.com>
2020-10-16 15:10:03 +0900

    Update

--- R/07_EffectPlot_2014.R
+++ R/07_EffectPlot_2014.R
@@ -10,7 +10,7 @@
 ################################################################################################
 # (1) NORTH CAROLINA CONTROLS
 ################################################################################################
-
+setwd("C:/Users/YUKI/Box/FromLaptop/Project/03_ColoradoVBM_BOB/VBM_analysis")
 library(tidyverse)
 library("ggsci")
 rm(list=ls())
@@ -18,31 +18,33 @@ rm(list=ls())
 {pdf("CATTPlot_2014.pdf", width=10, height=7)
 par(mar=c(1,3,1,1), mfrow=c(2,1))
 
-# Compute group proportions
-# dat <- read_csv("Stack_Colorado_NC_2012_2016_imputed.csv", col_types = cols(VoterID = col_character())) 
+
+# #Compute group proportions
+# dat <- read_csv("Stack_Colorado_NM_2012_2016.csv", col_types = cols(VoterID = col_character()))
 # datCO <- dat %>% filter(Place==1); rm(dat)
 # datCO %>% group_by(voted2010) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
+# datCO %>% mutate(ageGR = ifelse(age <= 40, 0, ifelse(age>=65, 2,1))) %>%
+#           group_by(ageGR) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 # datCO %>% group_by(estrace) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 # datCO %>% group_by(female) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 # datCO %>% group_by(democrat) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
-# datCO %>% mutate(ageGR = ifelse(age < 35, 0, ifelse(age>=65, 2,1))) %>%
-#           group_by(ageGR) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 
-prop = c(1, 0.848, 0.152,                # All, frequent, infrequent
-         0.156, 0.618, 0.226,            # U35, 35-65, O65 
-         0.730, 0.102, 0.105, 0.0536,    # White, black, hispanic, asian
-         0.522, 0.478,                   # Female, male
-         0.322, 0.678)                   # Democrat, non-Democrat
+prop = c(1, 0.685, 0.315,                # All, frequent, infrequent
+         0.240, 0.521, 0.239,            # U35, 35-65, O65 
+         0.730, 0.102, 0.105, 0.054,    # White, black, hispanic, asian
+         0.523, 0.477,                   # Female, male
+         0.323, 0.677)                   # Democrat, non-Democrat
 
 prop <- sqrt(prop)
 
-# CATTs from Table D.1 on Online Appendix
-att <- c(0.075, 0.073, 0.075, 0.042, 0.066, 0.115, 
-         0.067, 0.062, 0.091, 0.094, 0.068, 0.068, 0.049, 0.079)
-se  <- c(0.003, 0.003, 0.002, 0.004, 0.003, 0.003, 
-         0.003, 0.003, 0.003, 0.003, 0.003, 0.003, 0.003, 0.003)
 
-y <- seq(from=0, to=0.12, length=101)
+# CATTs from Table D.1 on Online Appendix
+att <- c(0.102, 0.095, 0.105, 0.096, 0.091, 0.119, 
+         0.097, 0.090, 0.120, 0.123, 0.096, 0.101, 0.079, 0.107)
+se  <- c(0.004, 0.003, 0.002, 0.004, 0.003, 0.003, 
+         0.003, 0.003, 0.004, 0.003, 0.004, 0.004, 0.004, 0.004)
+
+y <- seq(from=0.02, to=0.14, length=101)
 x <- seq(from=0, to=100, by=1)
 ratio <- 100/(14)
 cole <- "firebrick4"
@@ -75,7 +77,7 @@ for(i in 2:14){ arrows(y0=att[i]-1.96*se[i], y1=att[i]+1.96*se[i],
                        x0=ratio*i, x1=ratio*i, length=0, angle=0, col=cole, lwd=4)}
 for(i in 2:14){ points(ratio*i, att[i], pch=1, cex=4*prop[i], col="navy") }
 
-text(x=ratio*2.2,y=0.115, labels="North Carolina as Control", font=2, cex=1.5)
+text(x=ratio*2.2,y=0.135, labels="North Carolina as Control", font=2, cex=1.5)
 text(x=ratio*0.5,y=att[1]-0.01, labels="All Voter", font=2)
 text(x=ratio*2, y=att[2]+0.01, labels="Frequent Voter", font=2)
 text(x=ratio*3, y=att[3]+0.01, labels="Infrequent Voter", font=2)
@@ -96,26 +98,18 @@ text(x=ratio*14-1, y=att[14]+0.01, labels="Non-Dem", font=2)
 # (2) NEW MEXICO CONTROLS
 ################################################################################################
 
-prop = c(1, 0.848, 0.152,                # All, frequent, infrequent
-         0.156, 0.618, 0.226,            # U35, 35-65, O65 
-         0.730, 0.102, 0.105, 0.0536,    # White, black, hispanic, asian
-         0.522, 0.478,                   # Female, male
-         0.322, 0.678)                   # Democrat, non-Democrat
-
-prop <- sqrt(prop)
-
 # CATTs from Table D.1 on Online Appendix 
-att <- c(0.083, 0.099, 0.058, 0.051, 0.089, 0.100,
-         0.078, 0.100, 0.083, 0.105, 0.082, 0.077, 0.068, 0.087)
-se  <- c(0.003, 0.002, 0.002, 0.003, 0.003, 0.002, 
-         0.003, 0.006, 0.002, 0.006, 0.003, 0.003, 0.003, 0.003)
+att <- c(0.099, 0.112, 0.081, 0.109, 0.104, 0.102,
+         0.098, 0.116, 0.103, 0.125, 0.099, 0.102, 0.090, 0.107)
+se  <- c(0.003, 0.002, 0.002, 0.004, 0.003, 0.002, 
+         0.003, 0.007, 0.002, 0.008, 0.003, 0.003, 0.003, 0.003)
 
 
-y <- seq(from=0, to=0.12, length=101)
-x <- seq(from=0, to=100, by=1)
-ratio <- 100/(14)
-cole <- "firebrick4"
-pal <- pal_jco()(10)
+# y <- seq(from=0, to=0.12, length=101)
+# x <- seq(from=0, to=100, by=1)
+# ratio <- 100/(14)
+# cole <- "firebrick4"
+# pal <- pal_jco()(10)
 
 
 plot(y ~ x, type="n", xlab="", ylab="", xaxt="n")
@@ -144,7 +138,7 @@ for(i in 2:14){ arrows(y0=att[i]-1.96*se[i], y1=att[i]+1.96*se[i],
                        x0=ratio*i, x1=ratio*i, length=0, angle=0, col=cole, lwd=4)}
 for(i in 2:14){ points(ratio*i, att[i], pch=1, cex=4*prop[i], col="navy") }
 
-text(x=ratio*2,y=0.115, labels="New Mexico as Control", font=2, cex=1.5)
+text(x=ratio*2,y=0.135, labels="New Mexico as Control", font=2, cex=1.5)
 text(x=ratio*0.5,y=att[1]-0.01, labels="All Voter", font=2)
 text(x=ratio*2, y=att[2]+0.01, labels="Frequent Voter", font=2)
 text(x=ratio*3, y=att[3]+0.01, labels="Infrequent Voter", font=2)
