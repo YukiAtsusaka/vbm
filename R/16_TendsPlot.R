0786e4d9b65497c9741789f25ab6cc7049f255ab
Mona Lisa <yukia887@gmail.com>
2021-04-09 11:56:52 -0500

    Add trends plot for matched sample

--- R/16_TendsPlot.R
+++ R/16_TendsPlot.R
@@ -7,6 +7,9 @@
 
 library(tidyverse)
 setwd("data")
+####################################################
+# (1) RAW DATA
+####################################################
 nc14 <- read_csv("Stack_Colorado_NC_2012_2014.csv")
 nc16 <- read_csv("Stack_Colorado_NC_2012_2016.csv")
 nm14 <- read_csv("Stack_Colorado_NM_2012_2014.csv")
@@ -14,86 +17,104 @@ nm16 <- read_csv("Stack_Colorado_NM_2012_2016.csv")
 
 
 nc14_tr <- nc14 %>%
-           group_by(Year, voted2010, State) %>% 
+           group_by(Time, voted2010, Place) %>% 
            summarise(Turnout = mean(Vote))
 nc14_tr
 
-nc16_tr <- nc16 %>% group_by(Year, voted2010, State) %>% 
+nc16_tr <- nc16 %>% group_by(Time, voted2010, Place) %>% 
            summarise(Turnout = mean(Vote))
 nc16_tr
 
-nm14_tr <- nm14 %>% group_by(Year, voted2010, State) %>% 
+nm14_tr <- nm14 %>% group_by(Time, voted2010, Place) %>% 
            summarise(Turnout = mean(Vote))
 nm14_tr
 
-nm16_tr <- nm16 %>% group_by(Year, voted2010, State) %>% 
+nm16_tr <- nm16 %>% group_by(Time, voted2010, Place) %>% 
            summarise(Turnout = mean(Vote))
 nm16_tr
 
+rm(nc14, nc16, nm14, nm16); gc(); gc()
+
+####################################################
+# (2) MATCHED DATA
+####################################################
+
+high <- read_csv(here::here("data/NC14Sample/2_NC2014_SampleFreq.csv"))
+low <- read_csv(here::here("data/NC14Sample/3_NC2014_SampleInfreq.csv"))
+match_nc14 <- rbind(high, low) %>%
+              group_by(Time, voted2010, Place) %>% 
+              summarise(Turnout = mean(Vote))
+
+high <- read_csv(here::here("data/NC16Sample/2_NC2016_SampleFreq.csv"))
+low <- read_csv(here::here("data/NC16Sample/3_NC2016_SampleInfreq.csv"))
+match_nc16 <- rbind(high, low) %>%
+              group_by(Time, voted2010, Place) %>% 
+              summarise(Turnout = mean(Vote))
+
+high <- read_csv(here::here("data/NM14Sample/2_NM2014_SampleFreq.csv"))
+low <- read_csv(here::here("data/NM14Sample/3_NM2014_SampleInfreq.csv"))
+match_nm14 <- rbind(high, low) %>%
+              group_by(Time, voted2010, Place) %>% 
+              summarise(Turnout = mean(Vote))
+
+
+high <- read_csv(here::here("data/NM16Sample/2_NM2016_SampleFreq.csv"))
+low <- read_csv(here::here("data/NM16Sample/3_NM2016_SampleInfreq.csv"))
+match_nm16 <- rbind(high, low) %>%
+              group_by(Time, voted2010, Place) %>% 
+              summarise(Turnout = mean(Vote))
 
 
 
-{pdf(here::here("figures/TrendsPlot.pdf"), width=7, height=2)
-par(mfrow=c(1,4), mar=c(2,2,2,1), oma=c(0,0,1,0)+0.01, mgp=c(3,0.6,0))
+dt_list <- list(nc14_tr, nc16_tr, nm14_tr, nm16_tr,
+                match_nc14, match_nc16, match_nm14, match_nm16)
+vec_title <- c("CO v. NC, 2014", "CO v. NC, 2016", "CO v. NM, 2014", "CO v. NM, 2016")
+vec_title <- rep(vec_title,2)
 
-plot(nc14_tr$Year, nc14_tr$Turnout, type="n", xlab="", ylab="", xaxt="n", yaxt="n",ylim=c(0.1,1.1))
+####################################################
+# (3) PLOT THE SAVE THE GRAPH
+####################################################
+
+{pdf(here::here("figures/TrendsPlot.pdf"), width=7, height=3.5)
+par(mfrow=c(2,4), mar=c(2,2,2,1), oma=c(0,2,1,0)+0.01, mgp=c(3,0.6,0))
+
+for(i in 1:8){
+dt <- dt_list[[i]]
+
+
+
+plot(dt$Time, dt$Turnout, type="n", xlab="", ylab="", xaxt="n", yaxt="n",ylim=c(0.1,1.1))
 axis(2, at=seq(from=0.2,to=1, by=0.2), las=1)
-axis(1, at=unique(nc14_tr$Year), labels=c("Before", "After"), las=1, cex.lab=1.2)
-lines(nc14_tr$Year[nc14_tr$voted2010==0 & nc14_tr$State=="Colorado"], 
-      nc14_tr$Turnout[nc14_tr$voted2010==0 & nc14_tr$State=="Colorado"], type="b", pch=19, lty=2, col="firebrick4")
-lines(nc14_tr$Year[nc14_tr$voted2010==0 & nc14_tr$State=="North Carolina"], 
-      nc14_tr$Turnout[nc14_tr$voted2010==0 & nc14_tr$State=="North Carolina"], type="b", pch=17, lty=2)
-lines(nc14_tr$Year[nc14_tr$voted2010==1 & nc14_tr$State=="Colorado"], 
-      nc14_tr$Turnout[nc14_tr$voted2010==1 & nc14_tr$State=="Colorado"], type="b", pch=19, col="firebrick4")
-lines(nc14_tr$Year[nc14_tr$voted2010==1 & nc14_tr$State=="North Carolina"], 
-      nc14_tr$Turnout[nc14_tr$voted2010==1 & nc14_tr$State=="North Carolina"], type="b", pch=17)
-title("CO v. NC, 2014")
-points(2012.1, 0.7, pch=19, cex=0.8, col="firebrick4")
-points(2012.1, 0.62, pch=17, cex=0.8)
-text(2012.5, 0.7, labels="Treated", cex=0.8, col="firebrick4")
-text(2012.5, 0.62, labels="Control", cex=0.8)
-text(2013.5, 1, labels="High-propensity \nvoters", font=2, col="dimgray", cex=0.8)
-text(2012.6, 0.23, labels="Low-propensity  \nvoters", font=2, col="dimgray", cex=0.8)
+axis(1, at=unique(dt$Time), labels=c("Before", "After"), las=1, cex.lab=1.2)
 
-plot(nc16_tr$Year, nc16_tr$Turnout, type="n", xlab="", ylab="", xaxt="n", yaxt="n", ylim=c(0.1,1.1))
-axis(2, at=seq(from=0.2,to=1, by=0.2), las=1)
-axis(1, at=unique(nc16_tr$Year), labels=c("Before", "After"), las=1, cex.lab=1.2)
-lines(nc16_tr$Year[nc16_tr$voted2010==0 & nc16_tr$State=="Colorado"], 
-      nc16_tr$Turnout[nc16_tr$voted2010==0 & nc16_tr$State=="Colorado"], type="b", pch=19, lty=2, col="firebrick4")
-lines(nc16_tr$Year[nc16_tr$voted2010==0 & nc16_tr$State=="North Carolina"], 
-      nc16_tr$Turnout[nc16_tr$voted2010==0 & nc16_tr$State=="North Carolina"], type="b", pch=17, lty=2)
-lines(nc16_tr$Year[nc16_tr$voted2010==1 & nc16_tr$State=="Colorado"], 
-      nc16_tr$Turnout[nc16_tr$voted2010==1 & nc16_tr$State=="Colorado"], type="b", pch=19, col="firebrick4")
-lines(nc16_tr$Year[nc16_tr$voted2010==1 & nc16_tr$State=="North Carolina"], 
-      nc16_tr$Turnout[nc16_tr$voted2010==1 & nc16_tr$State=="North Carolina"], type="b", pch=17)
-title("CO v. NC, 2016")
+# HIGH-PROPENSITY VOTERS
+lines(dt$Time[dt$voted2010==1 & dt$Place==1],
+      dt$Turnout[dt$voted2010==1 & dt$Place==1], type="b", pch=19, col="firebrick4")
+lines(dt$Time[dt$voted2010==1 & dt$Place==0],
+      dt$Turnout[dt$voted2010==1 & dt$Place==0], type="b", pch=17)
+
+# LOW-PROPENSITY VOTERS
+lines(dt$Time[dt$voted2010==0 & dt$Place==1],
+      dt$Turnout[dt$voted2010==0 & dt$Place==1], type="b", pch=19, lty=2, col="firebrick4")
+lines(dt$Time[dt$voted2010==0 & dt$Place==0],
+      dt$Turnout[dt$voted2010==0 & dt$Place==0], type="b", pch=17, lty=2)
+title(vec_title[i])
+
+# TEXT ANNOTATION
+if(i==1){
+points(0.1, 0.7, pch=19, cex=0.8, col="firebrick4")
+points(0.1, 0.62, pch=17, cex=0.8)
+text(0.3, 0.7, labels="Treated", cex=0.8, col="firebrick4")
+text(0.3, 0.62, labels="Control", cex=0.8)
+text(0.7, 1, labels="High-propensity \nvoters", font=2, col="dimgray", cex=0.8)
+text(0.3, 0.23, labels="Low-propensity  \nvoters", font=2, col="dimgray", cex=0.8)
+mtext("Raw Data", side=2, line=2)
+}else if(i==5){
+mtext("Matched Sample", side=2, line=2)   
+} # END OF IF
 
 
-plot(nm14_tr$Year, nm14_tr$Turnout, type="n", xlab="", ylab="", xaxt="n", yaxt="n", ylim=c(0.1,1.1))
-axis(2, at=seq(from=0.2,to=1, by=0.2), las=1)
-axis(1, at=unique(nm14_tr$Year), labels=c("Before", "After"), las=1, cex.lab=1.2)
-lines(nm14_tr$Year[nm14_tr$voted2010==0 & nm14_tr$State=="Colorado"], 
-      nm14_tr$Turnout[nm14_tr$voted2010==0 & nm14_tr$State=="Colorado"], type="b", pch=19, lty=2, col="firebrick4")
-lines(nm14_tr$Year[nm14_tr$voted2010==0 & nm14_tr$State=="New Mexico"], 
-      nm14_tr$Turnout[nm14_tr$voted2010==0 & nm14_tr$State=="New Mexico"], type="b", pch=17, lty=2)
-lines(nm14_tr$Year[nm14_tr$voted2010==1 & nm14_tr$State=="Colorado"], 
-      nm14_tr$Turnout[nm14_tr$voted2010==1 & nm14_tr$State=="Colorado"], type="b", pch=19, col="firebrick4")
-lines(nm14_tr$Year[nm14_tr$voted2010==1 & nm14_tr$State=="New Mexico"], 
-      nm14_tr$Turnout[nm14_tr$voted2010==1 & nm14_tr$State=="New Mexico"], type="b", pch=17)
-title("CO v. NM, 2014")
-
-plot(nm16_tr$Year, nm16_tr$Turnout, type="n", xlab="", ylab="", xaxt="n", yaxt="n", ylim=c(0.1,1.1))
-axis(2, at=seq(from=0.2,to=1, by=0.2), las=1)
-axis(1, at=unique(nm16_tr$Year), labels=c("Before", "After"), las=1, cex.lab=1.2)
-lines(nm16_tr$Year[nm16_tr$voted2010==0 & nm16_tr$State=="Colorado"], 
-      nm16_tr$Turnout[nm16_tr$voted2010==0 & nm16_tr$State=="Colorado"], type="b", pch=19, lty=2, col="firebrick4")
-lines(nm16_tr$Year[nm16_tr$voted2010==0 & nm16_tr$State=="New Mexico"], 
-      nm16_tr$Turnout[nm16_tr$voted2010==0 & nm16_tr$State=="New Mexico"], type="b", pch=17, lty=2)
-lines(nm16_tr$Year[nm16_tr$voted2010==1 & nm16_tr$State=="Colorado"], 
-      nm16_tr$Turnout[nm16_tr$voted2010==1 & nm16_tr$State=="Colorado"], type="b", pch=19, col="firebrick4")
-lines(nm16_tr$Year[nm16_tr$voted2010==1 & nm16_tr$State=="New Mexico"], 
-      nm16_tr$Turnout[nm16_tr$voted2010==1 & nm16_tr$State=="New Mexico"], type="b", pch=17)
-title("CO v. NM, 2016")
+} # END OF LOOP
 
 dev.off()
 }
